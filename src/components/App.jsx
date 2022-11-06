3d0a1124681c1ba30d3c9914182e27fb5e15ff73
sotskayaea <92892144+sotskayaea@users.noreply.github.com>
2022-11-06 21:08:29 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,11 @@
-import Profile from './profile/Profile';
-import user from '../data/profile.json';
+
 
 
 
 export const App = () => {
-  const {username, tag, location, avatar, stats} = user;
   return (
     <>
-      <Profile
-              username={username}
-              tag={tag}
-              location={location}
-              avatar={avatar}
-              stats={stats}
-            />
+      Hello world
     </>
   )
 
